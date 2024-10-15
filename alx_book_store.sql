22fd2754fa089f7c0cdb8a67bf7a1b922a85dafa
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-15 19:01:31 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,1 +1,12 @@
+-- Database creation
+CREATE DATABASE alx_book_store;
 
+--Use the database
+USE alx_book_store;
+
+--Create Author table
+CREATE TABLE Authors (
+  author_id INT AUTO_INCREMENT,
+  author_name VARCHAR(21) NOT NULL,
+  PRIMARY KEY (author_id)
+);
