7396d6c9cd8710d1d061974fa6abef829443ed71
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-15 21:30:31 +0200

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,1 +1,17 @@
+import mysql.connector
+from mysql.connector import Error
 
+#Database connection parameters
+DB_HOST = 'localhost'
+DB_USER = 'your_username'
+DB_PASSWORD ='your_password'
+
+def create_database(db_host,db_user,db_password,db_name):
+  try:
+    #Establish database connection
+    connection = mysql.connector.connect(
+      host=db_host
+      user=db_user
+      password=db_password
+    )
+    
