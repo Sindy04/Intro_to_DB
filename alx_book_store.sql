4034d45107a9bb4871b43c67b27558f639aa6248
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-15 19:14:24 +0200

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -7,6 +7,19 @@ USE alx_book_store;
 --Create Author table
 CREATE TABLE Authors (
   author_id INT AUTO_INCREMENT,
-  author_name VARCHAR(21) NOT NULL,
+  author_name VARCHAR(215) NOT NULL,
   PRIMARY KEY (author_id)
 );
+
+--Create Book table
+CREATE TABLE Books(
+  book_id INT AUTO_INCREMENT,
+  title VARCHAR(130) NOT NULL,
+  author_id INT,
+  price DOUBLE NOT NULL,
+  publication_date DATE NOT NULL,
+  PRIMARY KEY (book_id),
+  FOREIGN KEY (author_id) REFERENCES
+  Authors(author_id)
+  );
+
