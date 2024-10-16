ca33551e0de0f77769bce815251943fc641d8b89
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-16 10:59:01 +0200

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,1 +1,15 @@
+--task_5.sql
 
+--insert data into customers table
+INSERT INTO customers(
+  customer_id,
+  customer_name,
+  email,
+  address
+  )
+VALUES (
+  1,
+  'Cole Baidoo',
+  'cbaidoo@sandtech.com',
+  '123 Happiness Ave'.
+  );
