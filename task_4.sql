7c9dda15d870238c0a4475f3dcd7cd52de9ffaa6
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-16 10:46:45 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,18 @@
+--task_4.sql
 
+--Use the information_schema database
+USE information_schema
+
+--Query the COLUMNS table for books table description
+SELECT
+COLUMN_NAME,
+DATA_TYPE,
+CHARACTER_MAXIMUM_LENGTH,
+NUMERIC_PRECISION,
+NUMERIC_SCALE,
+IS_NULLABLE
+FROM
+COLUMNS
+WHERE
+TABLE_SCHEMA = DATABASE()
+AND TABLE_NAME = 'books';
