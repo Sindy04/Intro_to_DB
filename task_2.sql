fa1c798bbde471c1624bb6091264ccc99ba085ab
Sindy04 <s.ndlovu@lab.clicklearning.org>
2024-10-16 09:01:47 +0200

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -30,3 +30,22 @@ CREATE TABLE Customers(
   PRIMARY KEY (customer_id)
   );
 
+--Create Orders table
+CREATE TABLE Orders(
+  order_id INT AUTO_INCREMENT,
+  customer_id INT,
+  order_date DATE NOT NULL,
+  PRIMARY KEY (order_id),
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+  );
+
+--Create Order_Details table
+CREATE TABLE Order_Details (
+  orderdetailid INT AUTO INCREMENT,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE NOT NULL,
+  PRIMARY KEY (orderdetailid),
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
+  );
